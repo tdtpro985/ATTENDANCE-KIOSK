896afd52a965f107a9b82d416a48e492701e4b30
tdtqrscanner <administrator@tdtpowersteel.com.ph>
2026-06-19 11:44:38 +0800

    fix: force EAS to re-upload backend config

--- src/config/backend.ts
+++ src/config/backend.ts
@@ -1,3 +1,3 @@
 const IP_ADDRESS = process.env.EXPO_PUBLIC_BACKEND_IP || '192.168.10.221';
 
-export const BACKEND_URL = `http://${IP_ADDRESS}:8080`;
+export const BACKEND_URL = `http://${IP_ADDRESS}:8080`;# Fri Jun 19 11:44:38 AM PST 2026
